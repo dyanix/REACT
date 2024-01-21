fe7bae668f4e397eea4fe4fdb08647a84073d1f5
dyanix <dyanixdhawale@gmail.com>
2024-01-22 02:54:36 +0530

    js

--- otp/src/components/Phoneotplog.jsx
+++ otp/src/components/Phoneotplog.jsx
@@ -46,8 +46,14 @@ const Phoneotplog = () => {
           value={phoneNumber}
           onChange={handlePhoneNumber}
           placeholder='Enter Phone Number'
+          style={{width:"210px" , height:"40px" ,margin:"5px" , textAlign:"center" , borderRadius:"10px" }}
+
         />
-        <button type='submit'>Submit</button>
+        <button 
+        type='submit'
+        style={{ width:"110px" , height:"42px" , textAlign:"center" , borderRadius:"10px" ,backgroundColor:"orchid"}}
+
+        >Submit</button>
       </form> : <div>
         <p>Enter OTP sent to {phoneNumber}</p>
        <OtpInput length={4} onOtpSubmit={onOtpSubmit}/>
--- otp/src/components/otpInput.jsx
+++ otp/src/components/otpInput.jsx
@@ -1,10 +1,65 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
-const OtpInput = () => {
+const OtpInput = ({length,onOtpSubmit}) => {
+    const [otp,setOtp] = useState(new Array(length).fill(""))
+    const inputRefs =  useRef([])
+
+    useEffect(()=>{
+        if(inputRefs.current[0]){
+            inputRefs.current[0].focus()
+
+        }
+    },[])
+
+    const handleChange = (event, index) => {
+
+    
+        const value = event.target.value;
+        console.log(value);
+    
+        if (isNaN(value)) return;
+    
+        const newOtp = [...otp];
+        //one input
+        newOtp[index] = value.substring(value.length - 1);
+        setOtp(newOtp);
+
+        const combinedotp = newOtp.join("")
+        if(combinedotp.length === length){
+
+            onOtpSubmit(combinedotp)
+        }
+        
+
+    };
+    
+    const handleClick = () =>{
+
+    }
+    const handleKeyDown = () =>{
+
+    }
+
    
   return (
 
-    <div>
+    <div >
+        {
+            otp.map((value,index)=>{
+                return (
+                    <input
+                    ref={(input)=>(inputRefs.current[index]=input)}
+                    style={{width:"40px" , height:"40px" ,margin:"5px" , textAlign:"center" , borderRadius:"10px"}}
+                    key={index}
+                    type='text'
+                    value={value}
+                    onChange={(event) => handleChange(event, index)}
+                    onClick={()=>handleClick(index)}
+                    onKeyDown={(e)=>handleKeyDown(index,e)}
+                    />
+                )
+            })
+        }
        
     </div>
     
