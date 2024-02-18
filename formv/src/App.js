5e3094faba2504f1bff9a479d3ea9eb687031d4c
dyanix <dyanixdhawale@gmail.com>
2024-02-18 19:29:46 +0530

    js

--- formv/src/App.js
+++ formv/src/App.js
@@ -1,24 +1,84 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [formData, setFormData] = useState({
+    Name: "",
+    Email: "",
+    Password: ""
+  })
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+
+  }
+
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setFormData({
+      ...formData,
+      [name]: value
+    })
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+
+      <form className='form' onSubmit={handleSubmit}>
+        <div>
+          <label>Name:</label>
+          <input
+            type='text'
+            placeholder='Your name here...'
+            name='Name'
+            value={formData.Name}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label>Email:</label>
+          <input
+            type='email'
+            placeholder='Your email address here...'
+            name='Email'
+            value={formData.Email}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+
+
+          <label>Password:</label>
+
+          <input
+            type={ "password" }
+            placeholder='Your password here...'
+            name='Password'
+            value={formData.Password}
+            onChange={handleChange}
+
+          />
+        
+    
+        </div>
+        <div>
+        <button>
+          Submit
+        </button>
+        </div>
+      </form>
+     
+
+
+
     </div>
+
   );
 }
 
